65ae74eab13f0976fbd0731487be75cc51148d7d
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2021-11-12 18:42:17 +0100

    dataframe coordonnées gps

--- asltam/creation dataframe.py
+++ asltam/creation dataframe.py
@@ -27,3 +27,27 @@ gare_2_2 = gare_2.dropna()
 
 # transformation coordonnées GPS
 coord = pd.read_csv("dataframe.csv", sep=',')
+
+coord_x = list(coord['x'])
+coord_y = list(coord['y'])
+coord_gps = []
+
+for i in range(len(coord_x)):
+    coord_gps.append(transform(inProj,outProj,coord['x'][i],coord['y'][i]))
+
+coord_gps_x = []
+coord_gps_y = []
+
+for i in range(len(coord_gps)):
+    coord_gps_x.append(coord_gps[i][0])
+    coord_gps_y.append(coord_gps[i][1])
+
+# Création dataframe des coordonnées gps
+
+for i in range(len(coord_gps_x)):
+    coord['x'] = np.where(coord['x'] == coord['x'][i], coord_gps_x[i] , coord['x'] )
+
+for i in range(len(coord_gps_y)):
+    coord['y'] = np.where(coord['y'] == coord['y'][i], coord_gps_y[i] , coord['y'] )
+
+coord.to_csv('dataframe_2.csv')
