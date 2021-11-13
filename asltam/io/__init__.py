f4f821f0c09ca3e1e54c1f97e423c6e1d4cb8986
alexcaapel@gmail.com <alexandre.capel@etu.umontpellier.fr>
2021-11-13 21:23:58 +0100

    Petits rangements

---
+++ asltam/io/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+url_geo = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/Development/asltam/dataframe_2.csv'
+
+path_target = os.path.join(os.geetcwd(),'data_geo.csv')
+
