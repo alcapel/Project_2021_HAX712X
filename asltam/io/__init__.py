85fb6a36315db6627ef39f6a6403dbb532c0232e
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2021-11-14 14:52:44 +0100

    correction de geetcwd en getcwd

--- asltam/io/__init__.py
+++ asltam/io/__init__.py
@@ -2,5 +2,5 @@ import os
 
 url_geo = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/Development/asltam/data/dataframe_2.csv'
 
-path_target = os.path.join(os.geetcwd(),'data_geo.csv')
+path_target = os.path.join(os.getcwd(),'data_geo.csv')
 
