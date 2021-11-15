51aac52f9b18307807d628d43c0579f25339687c
Malek-macow <67799348+Malek-macow@users.noreply.github.com>
2021-11-15 22:03:47 +0100

    Update itinéraire.py

--- asltam/itinéraire.py
+++ asltam/itinéraire.py
@@ -1,11 +1,13 @@
+
+
 #Nous commençons par importer la bibliothèque "pyroutelib3" avec la première ligne "from pyroutelib3 import Router"
 from pyroutelib3 import Router
 import folium
 
 #Les 4 lignes suivantes initialisent les coordonnées du point de départ et d'arrivée
-c = folium.Map(location=[43.610769,3.876716], zoom_start=15)
-coor_depart= [ 43.56321197997598, 3.832152851175532]
-coor_arrivee= [	43.17658759196942,	3.034479741724844] 
+c = folium.Map(location = [43.610769, 3.876716], zoom_start=15)
+coor_depart= [43.56321197997598, 3.832152851175532]
+coor_arrivee= [43.17658759196942, 3.034479741724844] 
 
 #Ajoutez un marqueur au point de départ et au point d'arriver
 folium.Marker(coor_depart,popup="Départ").add_to(c)
@@ -18,13 +20,13 @@ router = Router("car")
 depart = router.findNode(coor_depart[0], coor_depart[1])
 arrivee = router.findNode(coor_arrivee[0], coor_arrivee[1])
 
-#La ligne "status, route = router.doRoute(depart, arrivee)" permet d'effectuer le calcul de l'itinéraire.
+#Cette ligne permet d'effectuer le calcul de l'itinéraire.
 status, route = router.doRoute(depart, arrivee)
 
 #La dernière ligne est exécutée uniquement si le calcul est mené à son terme 
 if status == 'success':
     routeLatLons = list(map(router.nodeLatLon, route))
 
-#Afficher une ligne bleu sur la route trouvé
+#Afficher une ligne bleu sur la route 
 folium.PolyLine(routeLatLons, color="blue", weight=2.5, opacity=1).add_to(c)
 
