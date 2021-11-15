fee286bf11cff2ec023d28323ba5aa2a352216fa
Malek-macow <67799348+Malek-macow@users.noreply.github.com>
2021-11-15 21:56:37 +0100

    Update itinéraire.py

--- asltam/itinéraire.py
+++ asltam/itinéraire.py
@@ -3,13 +3,13 @@ from pyroutelib3 import Router
 import folium
 
 #Les 4 lignes suivantes initialisent les coordonnées du point de départ et d'arrivée
-c = folium.Map(location=[43.610769,3.876716], zoom_start=15)
-coor_depart= [ 43.56321197997598, 3.832152851175532]
-coor_arrivee= [	43.17658759196942,	3.034479741724844] 
+c = folium.Map(location=[43.610769, 3.876716], zoom_start=15)
+coor_depart = [43.56321197997598, 3.832152851175532]
+coor_arrivee = [43.17658759196942, 3.034479741724844] 
 
 #Ajoutez un marqueur au point de départ et au point d'arriver
-folium.Marker(coor_depart,popup="Départ").add_to(c)
-folium.Marker(coor_arrivee,popup="Arrivée").add_to(c)
+folium.Marker(coor_depart, popup="Départ").add_to(c)
+folium.Marker(coor_arrivee, popup="Arrivée").add_to(c)
 
 #Définir le véhicule qui sera utilisé pour effectuer le trajet. Dans notre cas, nous utilisons une voiture ("car")
 router = Router("car")
@@ -26,5 +26,5 @@ if status == 'success':
     routeLatLons = list(map(router.nodeLatLon, route))
 
 #Afficher une ligne bleu sur la route trouvé
-folium.PolyLine(routeLatLons, color="blue", weight=2.5, opacity=1).add_to(c)
+folium.PolyLine(routeLatLons, color = "blue", weight = 2.5, opacity=1).add_to(c)
 
