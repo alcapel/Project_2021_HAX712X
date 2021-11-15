e19f51602c81eb5941f51520fb1774c7af7bd8bf
Malek-macow <67799348+Malek-macow@users.noreply.github.com>
2021-11-15 16:41:13 +0100

    Add files via upload
    clean data price

---
+++ asltam/clean data price.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+price = pd.read_csv("price-data.csv", sep=';')
+price.info()
+
+#%%
+#Suppression
+del price['Vendargues']
+del price['Montpellier est']
+del price['Montpellier sud']
+del price['Le Boulou (peage sys ouvert)']
+del price['Montpellier ouest']
+del price['St-Jean-de-Vedas']
+del price['La Croix Daurade']
+del price['Borderouge']
+del price['Les lzards']
+del price['Sesquieres']
+del price['Montaudran']
+del price['Lasbordes']
+del price['Soupetrad']
+del price['La Roseraie']
+del price['Pamiers nord']
+del price['Pamiers sud']
+price=price.drop(index=[0,1,2,3,4,18,29,30,35,36,37,38,39,40,41,42])
+
+#%%
+price.to_csv('price_data2.csv')
