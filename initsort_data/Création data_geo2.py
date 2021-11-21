7ed87138ad3d1447a6e7c30cf41a9bd22a8de3d7
Malek-macow <67799348+Malek-macow@users.noreply.github.com>
2021-11-22 00:23:50 +0100

    Update Création data_geo2.py

--- initsort_data/Création data_geo2.py
+++ initsort_data/Création data_geo2.py
@@ -8,15 +8,19 @@ url = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/aslt
 path = os.path.join(os.getcwd(),'data_geo.csv')
 download(url, path, replace=False)
 geo = pd.read_csv('./data_geo.csv')
+
 # on réindexe pour avoir les données dans l'ordre
 geo = geo.drop(index=[0, 1, 2, 3, 4, 5, 6, 7, 20, 21, 29, 30, 
                       31, 32, 33, 34, 35, 36, 37, 38, 39, 
                       40, 41, 42, 43, 44, 45, 46, 50, 52])
+
 geo = geo.reset_index()
 del geo['index']
+
 geo = geo.reindex([0, 1, 2, 22, 3, 4, 5, 6, 7, 8,
                    9, 11, 10, 18, 17, 16, 15, 14, 
                    13, 19, 20, 21, 12])
+
 geo = geo.reset_index()
 del geo['index']
 
