7ddaeb4883fea541a9b963f79e6fb518b4eebd85
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-11-21 10:41:01 +0100

    Conflit

--- asltam/itinéraire.py
+++ asltam/itinéraire.py
@@ -6,8 +6,8 @@ import folium
 
 #Les 4 lignes suivantes initialisent les coordonnées du point de départ et d'arrivée
 c = folium.Map(location=[43.610769, 3.876716], zoom_start=15)
-coor_depart= [43.56321197997598, 3.832152851175532]
-coor_arrivee= [43.17658759196942, 3.034479741724844] 
+coor_depart = [43.56321197997598, 3.832152851175532]
+coor_arrivee = [43.17658759196942, 3.034479741724844] 
 
 #Ajoutez un marqueur au point de départ et au point d'arriver
 folium.Marker(coor_depart,popup="Départ").add_to(c)
