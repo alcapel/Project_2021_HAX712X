7ba033da9c2651e1676cac7fa4a1cf7eed86efbc
Malek-macow <67799348+Malek-macow@users.noreply.github.com>
2021-11-22 09:57:08 +0100

    Update distance.py

--- initsort_data/distance.py
+++ initsort_data/distance.py
@@ -10,24 +10,10 @@ def distance():
     Cette fonction nous sert à créer le dataframe des distances
     '''
     # chargement des données géographiques
-    url = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/data_geo.csv'
-    path = os.path.join(os.getcwd(),'data_geo.csv')
+    url = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/data_geo2.csv'
+    path = os.path.join(os.getcwd(),'data_geo2.csv')
     download(url, path, replace=False)
-    geo = pd.read_csv('./data_geo.csv')
-
-    # on réindexe pour avoir les données dans l'ordre
-    geo = geo.drop(index=[0, 1, 2, 3, 4, 5, 6, 7, 20, 21, 29, 30, 
-                        31, 32, 33, 34, 35, 36, 37, 38, 39, 
-                        40, 41, 42, 43, 44, 45, 46, 50, 52])
-
-    geo = geo.reset_index()
-    del geo['index']
-    geo = geo.reindex([0, 1, 2, 22, 3, 4, 5, 6, 7, 8,
-                    9, 11, 10, 18, 17, 16, 15, 14, 
-                    13, 19, 20, 21, 12])
-
-    geo = geo.reset_index()
-    del geo['index']
+    geo = pd.read_csv('./data_geo2.csv')
 
     # Initialisation des coordonnées
     latt = list(geo['Latt'])
