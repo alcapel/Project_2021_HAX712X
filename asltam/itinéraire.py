f22e02d8c74fc89a435350dad24d838d9b43f7b4
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2021-11-22 14:19:13 +0100

    pep8

--- asltam/itinéraire.py
+++ asltam/itinéraire.py
@@ -1,37 +1,44 @@
-#Nous commençons par importer la bibliothèque "pyroutelib3" avec la première ligne "from pyroutelib3 import Router"
+# Nous commençons par importer la bibliothèque "pyroutelib3"
+# qui nous permettra de calculer le trajet
+
 from pyroutelib3 import Router
 import folium
 
 
 coor_depart = [43.56321197997598, 3.832152851175532]
-coor_arrivee = [43.17658759196942, 3.034479741724844] 
+coor_arrivee = [43.17658759196942, 3.034479741724844]
+
 
 def trajet(coord_depart, coord_arrivee):
     """
     Cette fonction affiche une carte avec le trajet souhaité !
     """
-    #Les 3 lignes suivantes initialisent les coordonnées du point de départ et d'arrivée
+    # Les 3 lignes suivantes initialisent les coordonnées
+    # du point de départ et du point d'arrivée.
     c = folium.Map(location=[43.610769, 3.876716], zoom_start=15)
 
-    #Ajoutez un marqueur au point de départ et au point d'arriver
+    # Ajoute un marqueur au point de départ et au point d'arrivée.
     folium.Marker(coor_depart, popup="Départ").add_to(c)
     folium.Marker(coor_arrivee, popup="Arrivée").add_to(c)
 
-    #Définir le véhicule qui sera utilisé pour effectuer le trajet. Dans notre cas, nous utilisons une voiture ("car")
+    # Définir le véhicule qui sera utilisé pour effectuer le trajet.
+    # Dans notre cas, nous utilisons une voiture ("car").
     router = Router("car")
 
-    #Les 2 lignes suivantes recherchent le point de départ et le point d'arrivée.
+    # Les 2 lignes suivantes recherchent
+    # le point de départ et le point d'arrivée.
     depart = router.findNode(coor_depart[0], coor_depart[1])
     arrivee = router.findNode(coor_arrivee[0], coor_arrivee[1])
 
-    #Cette ligne permet d'effectuer le calcul de l'itinéraire.
+    # Cette ligne permet d'effectuer le calcul de l'itinéraire.
     status, route = router.doRoute(depart, arrivee)
 
-    #La dernière ligne est exécutée uniquement si le calcul est mené à son terme 
+    # La dernière ligne est exécutée uniquement
+    # si le calcul est mené à son terme.
     if status == 'success':
         routeLatLons = list(map(router.nodeLatLon, route))
 
-    #Afficher une ligne bleu sur la route 
+    # Afficher une ligne bleu sur la route.
     folium.PolyLine(routeLatLons, color="blue", weight=2.5, opacity=1).add_to(c)
 
     return c
