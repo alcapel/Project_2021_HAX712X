c6f7207cb984c192676b716669c85acd67b1d4bf
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2021-11-22 14:34:05 +0100

    Update distance.py

--- initsort_data/distance.py
+++ initsort_data/distance.py
@@ -5,13 +5,14 @@ from download import download
 import requests
 import json
 
+
 def distance():
     '''
     Cette fonction nous sert à créer le dataframe des distances
     '''
-    # chargement des données géographiques
+    # Chargement des données géographiques
     url = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/data_geo2.csv'
-    path = os.path.join(os.getcwd(),'data_geo2.csv')
+    path = os.path.join(os.getcwd(), 'data_geo2.csv')
     download(url, path, replace=False)
     geo = pd.read_csv('./data_geo2.csv')
 
@@ -21,18 +22,19 @@ def distance():
     coor = []
     n = len(long)
     for i in range(n):
-        coor.append([long[i],latt[i]])
-        
-    # Création de la matrice des distances
+        coor.append([long[i], latt[i]])
 
+    # Création de la matrice des distances
     D = np.zeros((n, n))
 
     for i in range(n):
-        # On ne construit que la diagonale supérieure car la matrice est diagonale
+        # La matrice est diagonale
+        # On ne construit donc que la diagonale supérieure
         x, y = coor[i]
         for j in range(i, n):
-            # On calcule les deux sens car la requête suit 
-            #le sens de la route : on prend la distance la plus petite qui sera la plus vraisemblable
+            # On calcule les deux sens car la requête suit
+            # le sens de la route : on prend la distance la plus petite,
+            # qui sera la plus vraisemblable
             x1, y1 = coor[j]
             r1 = requests.get(f"http://router.project-osrm.org/route/v1/car/{x},{y};{x1},{y1}?overview=false""")
             routes_1 = json.loads(r1.content)
@@ -40,7 +42,8 @@ def distance():
             r2 = requests.get(f"http://router.project-osrm.org/route/v1/car/{x1},{y1};{x},{y}?overview=false""")
             routes_2 = json.loads(r2.content)
             route_2 = routes_2.get("routes")[0]
-            D[i, j] = min(round(route_1['distance']/1000),round(route_2['distance']/1000))
+            D[i, j] = min(round(route_1['distance']/1000),
+                          round(route_2['distance']/1000))
 
     # On symétrise la matrice pour avoir le tableau de données complet
     Df = D + D.T
