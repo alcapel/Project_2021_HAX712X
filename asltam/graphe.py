afaa0110345ee1d157624db6c7eb142953bb7797
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2021-11-26 10:45:26 +0100

    Update graphe.py

--- asltam/graphe.py
+++ asltam/graphe.py
@@ -1,7 +1,18 @@
 import pandas as pd
 import networkx as nx
 
-price = pd.read_csv('price_data2.csv', index_col=' ')
 
+def plus_court_chemin(depart, arrivee):
+    ''' 
+    Cette fonction calcule le plus court chemin entre le départ et l'arrivée
+    '''
+    price = pd.read_csv('price_data2.csv', index_col=' ')
+    graphe = nx.Graph(incoming_graph_data = price)
+    chemin = nx.shortest_path(graphe, depart, arrivee, weight='weight')
+    print(chemin)
+
+
+price = pd.read_csv('price_data2.csv', index_col=' ')
 graphe = nx.Graph(incoming_graph_data = price)
-a = nx.shortest_path(graphe, 'Peage de Montpellier St-Jean', 'Bram', weight='weight')
+b = nx.minimum_spanning_tree(graphe, weight='weight')
+nx.draw(b, with_labels = True)
