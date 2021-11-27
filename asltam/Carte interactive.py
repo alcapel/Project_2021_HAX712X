f8c7b2ad9790e90a53a5474b58810041dd45729c
Malek-macow <67799348+Malek-macow@users.noreply.github.com>
2021-11-27 19:14:48 +0100

    Modification

--- asltam/Carte interactive.py
+++ asltam/Carte interactive.py
@@ -13,26 +13,32 @@ download(url, path, replace=False)
 
 geo = pd.read_csv('./data_geo2.csv')
 
-geo = geo.rename(columns={' Nom gare ':'gare'})
+#Modifier le nom de la colone "Nom gare" pour l'utiliser dans les deux lignes suivantes 
+geo = geo.rename(columns = {' Nom gare ':'gare'})
 
-depart = sorted(geo.gare.unique())
-arrivee = sorted(geo.gare.unique())
+#Crée une liste qui contient le nom de toutes les villes
+villes = sorted(geo.gare.unique())
 
 def trajet(DEPART,ARRIVEE):
     
-    if geo.loc[geo['gare']==DEPART].index[0] < geo.loc[geo['gare']==ARRIVEE].index[0] :
+    #Résolution du probléme de la distance différente entre l'aller et le retour avec une boucle 
+    if geo.loc[geo['gare'] == DEPART].index[0] < geo.loc[geo['gare'] == ARRIVEE].index[0] :
         
-        x =[geo['Long'][geo.loc[geo['gare']==DEPART].index[0]], 
-             geo['Latt'][geo.loc[geo['gare']==DEPART].index[0]]]
-        y =[geo['Long'][geo.loc[geo['gare']==ARRIVEE].index[0]], 
-             geo['Latt'][geo.loc[geo['gare']==ARRIVEE].index[0]]]
+        x =[geo['Long'][geo.loc[geo['gare'] == DEPART].index[0]], 
+             geo['Latt'][geo.loc[geo['gare'] == DEPART].index[0]]]
+        y =[geo['Long'][geo.loc[geo['gare'] == ARRIVEE].index[0]], 
+             geo['Latt'][geo.loc[geo['gare'] == ARRIVEE].index[0]]]
         
-        coor=[x, y]
+        coor = [x, y]
         
         client= openrouteservice.Client(key='5b3ce3597851110001cf62486f5564a064e34f3895221e5a0d9a2405')# Specify your personal API key
-                  
-        for i in range (0,len(coor)-1):
-            coord = coor[i],coor[i+1]
+        
+        c = folium.Map(location = [43.1837661,3.0042121],
+                      zoom_start = 10,
+                      control_scale = True)  
+                      
+        for i in range (0, len(coor)-1):
+            coord = coor[i], coor[i+1]
             res = client.directions(coord)
                        
             with(open('test.json','+w')) as f:
@@ -42,66 +48,61 @@ def trajet(DEPART,ARRIVEE):
                 decoded = convert.decode_polyline(geometry)
 
                 distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>" 
-                
-                c =folium.Map(location=[43.1837661,3.0042121],
-                       zoom_start=10, 
-                       control_scale=True
-                               
-                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt,max_width=300)).add_to(c)
+                      
+                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt, max_width = 300)).add_to(c)
                        
                 folium.Marker(coord[0][::-1], 
-                              popup=DEPART, 
-                              tooltip = "Click me!"
+                              popup = DEPART, 
+                              icon = folium.Icon(color = 'red', icon = 'car', prefix = 'fa')
                              ).add_to(c)
                        
                 folium.Marker(coord[1][::-1], 
-                              popup=ARRIVEE, 
-                              tooltip = "Click me!"
+                              popup = ARRIVEE, 
+                              icon = folium.Icon(color = 'red', icon = 'car', prefix = 'fa')
                              ).add_to(c)
         return c
                        
-    elif geo.loc[geo['gare']==DEPART].index[0] > geo.loc[geo['gare']==ARRIVEE].index[0] :
+    elif geo.loc[geo['gare'] == DEPART].index[0] > geo.loc[geo['gare'] == ARRIVEE].index[0] :
                        
-        y =[geo['Long'][geo.loc[geo['gare']==DEPART].index[0]],
-             geo['Latt'][geo.loc[geo['gare']==DEPART].index[0]]]
-        x =[geo['Long'][geo.loc[geo['gare']==ARRIVEE].index[0]],
-             geo['Latt'][geo.loc[geo['gare']==ARRIVEE].index[0]]]
+        y = [geo['Long'][geo.loc[geo['gare'] == DEPART].index[0]],
+             geo['Latt'][geo.loc[geo['gare'] == DEPART].index[0]]]
+        x = [geo['Long'][geo.loc[geo['gare'] == ARRIVEE].index[0]],
+             geo['Latt'][geo.loc[geo['gare'] == ARRIVEE].index[0]]]
                        
-        coor=[x, y]
+        coor = [x, y]
                        
         
-        client = openrouteservice.Client(key='5b3ce3597851110001cf62486f5564a064e34f3895221e5a0d9a2405')
+        client = openrouteservice.Client(key = '5b3ce3597851110001cf62486f5564a064e34f3895221e5a0d9a2405')
+        m = folium.Map(location = [43.1837661,3.0042121],
+                      zoom_start = 10,
+                      control_scale = True)
                                
         for i in range (0,len(coor)-1):
             coord = coor[i],coor[i+1]
             res = client.directions(coord)
                                
             with(open('test.json','+w')) as f:
-                f.write(json.dumps(res,indent=4, sort_keys=True))
+                f.write(json.dumps(res, indent = 4, sort_keys = True))
                                
                 geometry = client.directions(coord)['routes'][0]['geometry']
                 decoded = convert.decode_polyline(geometry)
 
-                distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"   
+                distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"  
                                
-                m= folium.Map(location=[43.1837661,3.0042121],
-                               zoom_start=10, 
-                               control_scale=True)
-                               
-                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt,max_width=300)).add_to(m)
+                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt, max_width = 300)).add_to(m)
                                
                 folium.Marker(coord[0][::-1], 
                               popup=DEPART, 
-                              tooltip = "Click me!"
+                              icon = folium.Icon(color = 'red', icon = 'car', prefix = 'fa')
                              ).add_to(m)
                                
                 folium.Marker(coord[1][::-1], 
-                              popup=ARRIVEE, 
-                              tooltip = "Click me!"
+                              popup = ARRIVEE, 
+                              icon = folium.Icon(color = 'red', icon = 'car' , prefix='fa')
                              ).add_to(m)
         return m
                                
     else :
         print("Veuilliez entrer deux villes différentes")  
                                
-interact(trajet, DEPART=depart, ARRIVEE=arrivee)
+interact(trajet, DEPART = villes, ARRIVEE = villes)
