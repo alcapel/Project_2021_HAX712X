163ae0d80abe7bd1013b4d060410679c5595dc95
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-11-28 15:52:09 +0100

    Problèmes import

--- asltam/io/pricedist.py
+++ asltam/io/pricedist.py
@@ -51,3 +51,20 @@ def get_index(data, name):
             return ind
         except Exception as a:
             print(f"Attention ! {a} n'appartient pas à la base de donnée.")
+
+def get_way(data_dist, start, target):
+    """
+    Renvoie une liste contenant les péages entre start et target.
+    Notre méthode se base sur le fait que l'algorithme de Kruskal nous
+    fournit un graphe du réseau routier avec seulement des arêtes
+    entre deux gares successives dont on déduit facilement la liste
+    avec un algorithme du plus court chemin (ici Dijkstra).
+    Attributs
+    ---------
+    data_dist : DataFrame, tableau de données sous forme de matrice de distance entre toutes les gares.
+    start : str, la gare de départ (doit être une élément de data_dist.columns).
+    target : str, la gare d'arrivée (doit être une élément de data_dist.columns).
+    """
+    G = nx.Graph(incoming_graph_data=data_dist)
+    a = nx.minimum_spanning_tree(G, weight='weight')
+    return nx.shortest_path(a, start, target, weight = 'weight')
