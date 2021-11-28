a52b1846711d4aba9897726508186179e35fa129
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-11-28 15:50:18 +0100

    Problèmes import

--- asltam/io/pricedist.py
+++ asltam/io/pricedist.py
@@ -21,3 +21,33 @@ def average_cost_list(data_price, data_dist):
     d = np.array(data_dist)[np.triu_indices(n, k=1)]
     return p/d
   
+def get_index(data, name):
+    """
+    Retourne la valeur de la position de la gare de péage dans le DataFrame
+
+    Attributs :
+    -----------
+    data : pd.DataFrame, dont les colonnes porte le nom des péages
+    name : str ou list, nom(s) d'une gare de péage de data
+    """
+    if type(name) == str:
+        try:
+            data[name] + 0 == 1
+            i = 0
+            while i < len(data) and name != data.columns[i]:
+                i += 1
+            return i
+        except Exception as a:
+            print(f"Attention ! {a} n'appartient pas à la base de donnée.")
+    elif type(name) == list:
+        ind = []
+        try:
+            data[name] + 0 == 1
+            for j in range(len(name)):
+                i = 0
+                while i < len(data) and name[j] != data.columns[i]:
+                    i += 1
+                ind.append(i)
+            return ind
+        except Exception as a:
+            print(f"Attention ! {a} n'appartient pas à la base de donnée.")
