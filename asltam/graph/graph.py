20d2c133981cbf0503ecb45bf2f7eada4539b592
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-11-28 15:50:59 +0100

    Update graph.py

--- asltam/graph/graph.py
+++ asltam/graph/graph.py
@@ -1,24 +1,7 @@
 import networkx as nx
 from itertools import combinations
 from asltam.io.load_price import load_price
-from asltam.price_distribution.distribution import get_index
-
-def get_way(data_dist, start, target):
-    """
-    Renvoie une liste contenant les péages entre start et target.
-    Notre méthode se base sur le fait que l'algorithme de Kruskal nous
-    fournit un graphe du réseau routier avec seulement des arêtes
-    entre deux gares successives dont on déduit facilement la liste
-    avec un algorithme du plus court chemin (ici Dijkstra).
-    Attributs
-    ---------
-    data_dist : DataFrame, tableau de données sous forme de matrice de distance entre toutes les gares.
-    start : str, la gare de départ (doit être une élément de data_dist.columns).
-    target : str, la gare d'arrivée (doit être une élément de data_dist.columns).
-    """
-    G = nx.Graph(incoming_graph_data=data_dist)
-    a = nx.minimum_spanning_tree(G, weight='weight')
-    return nx.shortest_path(a, start, target, weight = 'weight')
+from asltam.io.pricedist import get_index, get_way
 
 def get_subgraph_under_k(data, k):
     """
