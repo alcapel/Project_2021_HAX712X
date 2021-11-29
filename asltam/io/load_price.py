8da03d4afa2c4c8c3267b21258c560ffbb0b01e2
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-11-29 19:05:05 +0100

    Update load_price.py

--- asltam/io/load_price.py
+++ asltam/io/load_price.py
@@ -6,8 +6,8 @@ class load_price :
 	def __init__(self, url = url_price, target = path_price):
 		download(url, target, replace = False)
 
-	def save_as_price():
-		df_price = pd.read_csv(path_price)
+	def save_as_price(index=None):
+		df_price = pd.read_csv(path_price, index_col=index)
 		return df_price
 	def subdata_price(data, index):
         	return data.iloc[index][data.columns[index]]
