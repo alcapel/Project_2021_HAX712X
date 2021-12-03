3835540e2bd29b0c757ded6a5306d72377acd914
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-12-03 15:19:13 +0100

    Update __init__.py

--- asltam/io/__init__.py
+++ asltam/io/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-url_price = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/price_dataf.csv'
+url_price = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/price_dataf2.csv'
 url_dist = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/data_dist.csv'
 path_price = os.path.join(os.getcwd(), 'data_price.csv')
 path_dist = os.path.join(os.getcwd(), 'data_dist.csv')
