9f7ce2efd2d572c82f7d2f96a8485abfdeabee2b
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2021-12-03 17:59:46 +0100

    pep8

--- asltam/tests/test_asltam.py
+++ asltam/tests/test_asltam.py
@@ -1,55 +1,65 @@
 import sys
 import os.path
 import download
+import pandas as pd
+import networkx as nx
+import numpy as np
+import asltam as atm
+from asltam.io.pricedist import *
+from asltam.graph.graph import get_subgraph_under_k
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
-import asltam as am
-from asltam.io.pricedist import *
-from asltam.graph.graph import get_subgraph_under_k
-import networkx as nx
-import numpy as np
-
-import pandas as pd
 
 def test_get_index():
-    a = am.load_price().save_as_price(index=' ')
+    a = atm.load_price().save_as_price(index=' ')
     assert get_index(data=a, name='SETE') == 1
 
+
 def test_get_index():
-    a = am.load_price().save_as_price(index=' ')
+    a = atm.load_price().save_as_price(index=' ')
     assert get_index(data=a, name='SETE') == 1
-    
+
+
 def test_get_index_list():
-    a = am.load_price().save_as_price(index=' ')
+    a = atm.load_price().save_as_price(index=' ')
     assert get_index(data=a, name=['SETE', 'LEZIGNAN']) == [1, 13]
 
+
 def test_get_way():
-    a = am.load_dist().save_as_dist(index=' Nom gare ')
+    a = atm.load_dist().save_as_dist(index=' Nom gare ')
     assert get_way(data_dist=a, start='MONTPELLIER', target='AGDE') == ['MONTPELLIER', 'SETE', 'AGDE']
 
+
 def test_average_cost():
-    a = am.load_price().save_as_price(index=' ')
-    b = am.load_dist().save_as_dist(index=' Nom gare ')
-    assert np.allclose(average_cost(data_price=am.load_price.subdata_price(a, [0, 1]),
-    data_dist=am.load_dist.subdata_dist(b,[0,1])),
-     np.array([[0, 1.6/17.0], [1.6/17.0 , 0]]))
+    a = atm.load_price().save_as_price(index=' ')
+    b = atm.load_dist().save_as_dist(index=' Nom gare ')
+    assert np.allclose(average_cost(data_price=atm.load_price.subdata_price(a, [0, 1]),
+    data_dist=atm.load_dist.subdata_dist(b, [0,1])),
+    np.array([[0, 1.6/17.0], [1.6/17.0, 0]]))
+
 
 def test_average_cost_list():
-    a = am.load_price().save_as_price(index=' ')
-    b = am.load_dist().save_as_dist(index=' Nom gare ')
-    assert np.allclose(average_cost_list(data_price=am.load_price.subdata_price(a, [0, 1]),
-    data_dist=am.load_dist.subdata_dist(b, [0, 1])),
-     np.array([1.6/17.0]))
+    a = atm.load_price().save_as_price(index=' ')
+    b = atm.load_dist().save_as_dist(index=' Nom gare ')
+    assert np.allclose(
+                       average_cost_list(data_price=atm.load_price.subdata_price(a, [0, 1]),
+                       data_dist=atm.load_dist.subdata_dist(b, [0, 1])),
+                       np.array([1.6/17.0]))
+
 
 def test_get_subgraph_under_k():
-    a = am.load_price().save_as_price(index=' ')
-    g = get_subgraph_under_k(am.load_price.subdata_price(a, range(4)), 1)
+    a = atm.load_price().save_as_price(index=' ')
+    g = get_subgraph_under_k(atm.load_price.subdata_price(a, range(4)), 1)
     assert [tuple(g[0].nodes), tuple(g[1].nodes), tuple(g[2].nodes)] == [('MONTPELLIER', 'BEZIERS CABRIALS (SORTIE)'),
-    ('MONTPELLIER', 'SETE', 'BEZIERS CABRIALS (SORTIE)'), ('MONTPELLIER', 'AGDE', 'BEZIERS CABRIALS (SORTIE)')]
+                                                                        ('MONTPELLIER', 'SETE', 'BEZIERS CABRIALS (SORTIE)'),
+                                                                        ('MONTPELLIER', 'AGDE', 'BEZIERS CABRIALS (SORTIE)')]
+
 
 def test_kmin_cost_out():
-    a = am.load_price().save_as_price(index=' ')
-    b = am.load_dist.save_as_dist(index=' Nom gare ')
-    assert am.kmin_cost_out(data_price=a, data_dist=b, start='MONTPELLIER',
-                         target='BEZIERS CABRIALS (SORTIE)', k =1)==(['MONTPELLIER', 'AGDE', 'BEZIERS CABRIALS (SORTIE)'], 4.6)
+    a = atm.load_price().save_as_price(index=' ')
+    b = atm.load_dist.save_as_dist(index=' Nom gare ')
+    assert am.kmin_cost_out(data_price=a,
+                            data_dist=b,
+                            start='MONTPELLIER',
+                            target='BEZIERS CABRIALS (SORTIE)', k=1) == (['MONTPELLIER', 'AGDE', 'BEZIERS CABRIALS (SORTIE)'], 4.6)
