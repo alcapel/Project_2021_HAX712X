b3e328393f141e1f180823aeb7597c12a64a85ea
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-12-04 21:02:05 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,14 +3,19 @@ from ipywidgets import interact, fixed
 
 prix = atm.load_price().save_as_price(index=' ')
 dist = atm.load_dist().save_as_dist(index=' Nom gare ')
+geo = atm.load_geo().save_as_geo(index=' Nom gare ')
+geo = geo[['Latt', 'Long']]
 
+#%% Construction de la carte intéractive
+interact(trajet, DEPART=list(geo.index), ARRIVEE=list(geo.index), 
+         data_geo=fixed(geo), data_price=fixed(prix), data_dist=fixed(dist))
 
-# Affichage des distributions avec interact
+#%% Affichage des distributions avec interact
 interact(atm.kde_gare, all = True, data_dist = fixed(dist),
          data_price = fixed(prix), start = prix.columns, 
          target = prix.columns, bw = (0.05,2,0.01))
 
-# Recherche des trajets optimaux
+#%% Recherche des trajets optimaux
 start = input(f"Donnez une gare de départ parmi les gares suivantes : {list(prix.index)}.")
 target = input(f"Donnez une gare d'arrivée parmi les gares suivantes : {list(prix.index)}.")
 n = input("Rentrez le nombre de sortie que vous vous accordez :")
