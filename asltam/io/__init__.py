887b9500385cfd9f245fbb9952e073200f28a648
Malek-macow <67799348+Malek-macow@users.noreply.github.com>
2021-12-04 17:01:34 +0100

    Update __init__.py

--- asltam/io/__init__.py
+++ asltam/io/__init__.py
@@ -2,5 +2,7 @@ import os
 
 url_price = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/price_dataf2.csv'
 url_dist = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/data_dist.csv'
+url_geo = 'https://raw.githubusercontent.com/Eldohrim/Project_2021_HAX712X/main/asltam/data/data_geo2.csv'
 path_price = os.path.join(os.getcwd(), 'data_price.csv')
 path_dist = os.path.join(os.getcwd(), 'data_dist.csv')
+path_geo = os.path.join(os.getcwd(), 'data_geo2.csv')
