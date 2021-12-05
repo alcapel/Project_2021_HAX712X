d4d00dcec0513e2ff80773baeac0b9ba98670f66
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-12-05 18:28:42 +0100

    Update graph.py

--- asltam/graph/graph.py
+++ asltam/graph/graph.py
@@ -9,32 +9,31 @@ start = time.time()
 
 
 def get_subgraph_under_k(data, k):
-	"""
-	Renvoie une liste contenant tous les 
-	sous-graphes induits par data, de taille 
-	inférieure ou égal à k.
+    """
+    Renvoie une liste contenant tous les 
+    sous-graphes induits par data, de taille 
+    inférieure ou égal à k.
 
-	Paramètres
-	----------
-	data : DataFrame or array.
-	
-	k : int.
-		Déterminant la taille maximale des 
-		sous-graphes.
+    Paramètres
+    ----------
+    data : DataFrame or array.
 
-	Return
-	------
-	L : list of graph.
+    k : int.
+        Déterminant la taille maximale des 
+        sous-graphes.
 
-	"""
-	n = len(data)
-	graphes = []
-	for j in range(k+1):
-	for i in combinations(range(1, n-1), j):
-	    sub = [0] + list(i) + [n-1]
-	    g = load_price.subdata_price(data, sub)
-	    graphes.append(nx.Graph(incoming_graph_data=g))
-	return graphes
+    Return
+    ------
+    L : list of graph.
+    """
+    n = len(data)
+    graphes = []
+    for j in range(k+1):
+        for i in combinations(range(1, n-1), j):
+            sub = [0] + list(i) + [n-1]
+            g = load_price.subdata_price(data, sub)
+            graphes.append(nx.Graph(incoming_graph_data=g))
+    return graphes
 
 end = time.time()
 print("Temps passé pour exécuter get_subgraph_under_k : {0:.5f} s.".format(end - start))
@@ -44,50 +43,50 @@ start = time.time()
 
 
 def kmin_cost_out(data_price, data_dist, start, target, k):
-	"""
-	Calcule le trajet de frais minimum avec la 
-	contrainte d'au maximum k entrées/sorties 
-	d'autoroute. Renvoie le prix, et les gares
-	de sortie. Attention les tableaux de prix et de
-	distances doivent être compatibles.
-	
-	Paramètres
-	----------
-	data_price : DataFrame.
-		Tableau des prix.
-	
-	data_dist : DataFrame.
-		Tableau des distances.
-	
-	start : str
-		Gare d'entrée.
-	
-	target : str,$.
-		Gare de sortie.
-	
-	k : int.
-		Nombre maximum d'entrées/sorties.
-	
-	Returns
-	-------
-	L, opt : list, float.
-		Renvoie la liste des sorties à prendre
-		et le coût total du trajet.
+    """
+    Calcule le trajet de frais minimum avec la 
+    contrainte d'au maximum k entrées/sorties 
+    d'autoroute. Renvoie le prix, et les gares
+    de sortie. Attention les tableaux de prix et de
+    distances doivent être compatibles.
 
-	"""
-	a = get_way(data_dist, start, target)
-	road = load_price.subdata_price(data_price, get_index(data_dist, a))
-	opt = nx.Graph(incoming_graph_data=road).size(weight='weight')
-	graphes = get_subgraph_under_k(road, k)
-	i_opt = 0
-	for i in range(len(graphes)):
-		path = nx.minimum_spanning_tree(graphes[i], weight='weight')
-		pds = path.size(weight='weight')
-		if pds < opt:
-		    i_opt = i
-		    opt = pds
-	g = list(graphes[i_opt].nodes)
-	return g, opt
+    Paramètres
+    ----------
+    data_price : DataFrame.
+        Tableau des prix.
+
+    data_dist : DataFrame.
+        Tableau des distances.
+
+    start : str
+        Gare d'entrée.
+
+    target : str,$.
+        Gare de sortie.
+
+    k : int.
+        Nombre maximum d'entrées/sorties.
+
+    Returns
+    -------
+    L, opt : list, float.
+        Renvoie la liste des sorties à prendre
+        et le coût total du trajet.
+
+    """
+    a = get_way(data_dist, start, target)
+    road = load_price.subdata_price(data_price, get_index(data_dist, a))
+    opt = nx.Graph(incoming_graph_data=road).size(weight='weight')
+    graphes = get_subgraph_under_k(road, k)
+    i_opt = 0
+    for i in range(len(graphes)):
+        path = nx.minimum_spanning_tree(graphes[i], weight='weight')
+        pds = path.size(weight='weight')
+        if pds < opt:
+            i_opt = i
+            opt = pds
+    g = list(graphes[i_opt].nodes)
+    return g, opt
 
 end = time.time()
 print("Temps passé pour exécuter kmin_cost_out: {0:.5f} s.".format(end - start))
