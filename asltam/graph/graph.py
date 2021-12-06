b2783744181022e0a8866162895956f27f64704f
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-12-06 19:41:59 +0100

    Update graph.py

--- asltam/graph/graph.py
+++ asltam/graph/graph.py
@@ -14,17 +14,12 @@ def get_subgraph_under_k(data, k):
     sous-graphes induits par data, de taille 
     inférieure ou égal à k.
 
-    Paramètres
-    ----------
-    data : DataFrame or array.
+    :param DataFrame or Array data:
 
-    k : int.
-        Déterminant la taille maximale des 
-        sous-graphes.
+    :param int k : Déterminant la taille maximale des 
+    sous-graphes.
 
-    Return
-    ------
-    L : list of graph.
+    :return list L: Une liste de graphe.
     """
     n = len(data)
     graphes = []
@@ -52,27 +47,31 @@ def kmin_cost_out(data_price, data_dist, start, target, k):
 
     Paramètres
     ----------
-    data_price : DataFrame.
-        Tableau des prix.
+    :param DataFrame data_price: Tableau des prix.
 
-    data_dist : DataFrame.
-        Tableau des distances.
+    :param DataFrame data_dist: Tableau des distances.
 
-    start : str
-        Gare d'entrée.
+    :param str start: Gare d'entrée.
 
-    target : str,$.
-        Gare de sortie.
+    :param str target: Gare de sortie.
 
-    k : int.
-        Nombre maximum d'entrées/sorties.
+    :param int k: Nombre maximum d'entrées/sorties.
 
-    Returns
-    -------
-    L, opt : list, float.
-        Renvoie la liste des sorties à prendre
-        et le coût total du trajet.
-
+    :returns list L: La liste des sorties à prendre.
+    :returns float opt: Renvoie le coût totaldu trajet.
+    
+    :examples:
+    .. code:: python
+    
+        >>> import asltam as atm
+        >>> prix = atm.load_price().save_as_price(index=' ')
+        >>> dist = atm.load_dist().save_as_dist(index=' Nom gare ')
+        >>> start = 'MONTPELLIER'
+        >>> target = 'LEUCATE'
+        >>> g, opt = atm.kmin_cost_out(prix, dist, start, target, 4)
+        >>> print(g, opt)
+        ['MONTPELLIER', 'SETE', 'AGDE', 'BEZIERS OUEST', 'LEUCATE'] 9.0
+        
     """
     a = get_way(data_dist, start, target)
     road = load_price.subdata_price(data_price, get_index(data_dist, a))
