fe54798d1ab5bb84fb0b4a3072ae2dc312ce6c3b
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-12-08 20:42:49 +0100

    Update __init__.py

--- asltam/__init__.py
+++ asltam/__init__.py
@@ -1,8 +1,8 @@
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from .io.load_geo import load_geo
 from .io.load_dist import load_dist
 from .io.load_price import load_price
 from .map.carte_interactive import trajet
-from .price_distribution.distribution import kde_gare
+from .price_distribution.distribution import *
 from .graph.graph import kmin_cost_out
