20dc2053a94a7fc0df79a436a85cb34dcd3d7c9d
Eldohrim <86468358+Eldohrim@users.noreply.github.com>
2021-12-09 22:07:08 +0100

    Update distribution.py

--- asltam/price_distribution/distribution.py
+++ asltam/price_distribution/distribution.py
@@ -21,39 +21,39 @@ def kde_gare(
     Affiche la distribution des prix au kilomètre entre deux 
     gares de péages sur un trajet donné (ou sur tout le 
     réseau). 
-    
-    .. warning:: 
-            Attention, les données de distance et de prix 
-            doivent être compatible pour pouvoir avoir 
-            des résultats cohérents. 
-            
-            Voir la documentation pour la compatibilité.
+
+    .. warning:: Attention, les données de distance et de prix
+            doivent être compatible pour pouvoir avoir des résultats cohérents. 
+            Voir la documentation pour la :ref:`Compatibilité`.
 
     :param bool all: Si True affiche la distribution sur le DataFrame entier.
-    
+
     :param DataFrame data_price: Tableaux de données de prix entre les gares de péage.
-    
+
     :param DataFrame data_dist: Tableaux de données des distances entre les gares de péage.
-    
+
     :param str start: Nom de la première gare.
-    
+
     :param str target: Nom de la seconde gare.
-    
+
     :param float bw: Voir doc bw_adjust de seaborn.
-    
-    :returns:
-    None.
-    
-    :examples:
-    .. code:: python
-    	
-            >>> import asltam as atm
-            >>> prix = atm.load_price().save_as_price(index=' ')
-            >>> dist = atm.load_dist().save_as_dist(index=' Nom gare ')
-    	>>> atm.kde_gare(True, prix, dist, 1)
+
+    :returns: None.
+
+    :examples: .. code:: python
+                    >>> import asltam as atm
+                    >>> prix = load_price().save_as_price(index=' ')
+                    >>> dist = load_dist().save_as_dist(index=' Nom gare ')
+                    >>> atm.kde_gare(True, prix, dist, 1)
     """
     if all:
+        # Cas où l'on veut  afficher la distribution
+        # avec tous le tableau de données.
+
+        # Création de la liste des prix moyens.
         m = average_cost_list(data_price, data_dist)
+
+        # Affichage du KDE avec ces données.
         fig, ax = plt.subplots(1, 1, figsize=(5, 5))
         sns.kdeplot(m, shade=True, cut=0, bw_adjust=bw)
         plt.ylabel("Density level")
@@ -64,18 +64,25 @@ def kde_gare(
     elif start == target:
         print("Ce sont les mêmes gares : il n'y a rien à afficher.")
     else:
+        # Calcul des numéros d'index (utile pour utiliser avec d'autres données)
         i = get_index(data_price, start)
         j = get_index(data_price, target)
+
+        # pour avoir juste les gares qui nous intéresse,
+        # il nous faut le nom des gare du trajet.
         route = get_way(data_dist, data_dist.columns[i], data_dist.columns[j])
         route_ind = get_index(data_dist, route)
 
         if len(route_ind) == 2:
+            # c'est le cas où il n'y a qu'un seul trajet possible.
             print(
                 "Il n'y a qu'un seul trajet ",
                 f"possible entre \n{data_price.columns[i]} et ",
                 data_price.columns[j], ".",
                 f"\nLe trajet coûte {round(data_price.iloc[i][j]/data_dist.iloc[i][j], 3)}€/km")
         else:
+            # on crée des sous tableaux sur la portion qui nous intéresse
+            # et on refait ce que l'on a fait dans le cas all=True.
             p = load_price.subdata_price(data_price, route_ind)
             d = load_dist.subdata_dist(data_dist, route_ind)
             m = average_cost_list(p, d)
@@ -95,41 +102,13 @@ def mean_gare(data):
 
     :param DataFrame data: Tableau de données numériques.
 
-    :return:
-        **b** (*pd.core.Series.Series*) Le vecteur aux coordonnées moyennes.
+    :return: * **b** (*pd.core.Series.Series*) - Le vecteur aux coordonnées moyennes.
 
-    :example:  
-        .. code:: python
-
-                >>> import asltam as atm
-                >>> # affichons les prix moyens par gare
-                >>> prix = atm.load_price().save_as_price(index=' ')
-                >>> print(atm.mean_gare(prix))
-
-        MONTPELLIER                    13.339130\n
-        SETE                           11.247826\n
-        AGDE                            9.239130\n
-        BEZIERS CABRIALS (SORTIE)       8.430435\n
-        BEZIERS OUEST                   7.704348\n
-        NARBONNE EST                    6.878261\n
-        NARBONNE SUD                    6.339130\n
-        SIGEAN                          7.108696\n
-        LEUCATE                         7.543478\n
-        PERPIGNAN NORD                  9.391304\n
-        PERPIGNAN SUD                   9.830435\n
-        BOULOU (FERME)                 11.600000\n
-        LE PERTHUS-LE BOULOU           12.708696\n
-        LEZIGNAN                        6.408696\n
-        CARCASSONNE EST                 7.521739\n
-        CARCASSONNE OUEST               7.660870\n
-        BRAM                            8.608696\n
-        CASTELNAUDARY                   9.086957\n
-        VILLEFRANCHE DE LAURAGAIS      10.213043\n
-        NAILLOUX                       10.843478\n
-        MAZERES                        12.152174\n
-        PAMIER                         12.969565\n
-        TOULOUSE SUD-OUEST (SORTIE)    12.660870\n
-        dtype: float64
+    :example: .. code:: python
+                    >>> import asltam as atm
+                    >>> # affichons les prix moyens par gare
+                    >>> prix = atm.load_price().save_as_price(index=' ')
+                    >>> print(atm.mean_gare(prix))
     """
     return data.mean()
 
@@ -141,27 +120,26 @@ def swarm_gare_price(data_price, name):
     :param DataFrame data_price: Tableau de données des prix.
 
     :param list name: Liste d'index de data_price.
-    
+
     :return: None.
 
-    :examples:
-        .. code:: python
-        
+    :examples: .. code:: python 
                 >>> import asltam as atm
                 >>> prix = atm.load_price().save_as_price(index=' ')
                 >>> atm.swarm_gare_price(prix, ['MONTPELLIER', 'BRAM', 'CASTELNAUDARY'])
-    
     """
-    name_ind = get_index(name)
+    # Calcul des numéros d'index (utile pour utiliser avec d'autres données).
+    name_ind = get_index(data_price, name)
     p = data_price[data_price.columns[name_ind]]
     sen = ' '
+
     for i in range(len(name)):
+        # Pour le titre du graphique.
         sen+= name[i] + ', '
 
+    # Affichage du swarmplot.
     fig = px.strip(p, orientation='h', hover_name=p.index, 
                         labels = { "value" : "Prix (en €)",
                                     "variable" :"Villes"}, 
                         title =f"Swarmplot des prix entre {sen}")
     fig.show()
-
-
